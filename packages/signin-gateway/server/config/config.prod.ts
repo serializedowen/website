28191502432cbe9af29b7a414c2f816c3451eb00
serializedowen <wjh199455@gmail.com>
2020-10-10 22:08:55 +0800

    added prod config

--- packages/signin-gateway/server/config/config.prod.ts
+++ packages/signin-gateway/server/config/config.prod.ts
@@ -1,6 +1,6 @@
-import { EggAppConfig, PowerPartial } from 'egg';
-
+import { EggAppConfig, PowerPartial } from "egg";
+import sqlconfig from "./sql.config.prod";
 export default () => {
   const config: PowerPartial<EggAppConfig> = {};
-  return config;
+  return { ...config, sequelize: sqlconfig };
 };
---
+++ packages/signin-gateway/server/config/sql.config.prod.ts
@@ -0,0 +1,8 @@
+export default {
+  username: "root",
+  password: "123456",
+  database: "database_production",
+  host: "localhost",
+  port: 3306,
+  dialect: "mysql",
+};
