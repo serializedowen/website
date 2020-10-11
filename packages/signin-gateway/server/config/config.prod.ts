cd1386244996a39a9fa26e8cc32f87b1563c019f
serializedowen <wjh199455@gmail.com>
2020-10-11 20:25:22 +0800

    fix: set top domain cookie

--- packages/signin-gateway/server/config/config.prod.ts
+++ packages/signin-gateway/server/config/config.prod.ts
@@ -2,5 +2,8 @@ import { EggAppConfig, PowerPartial } from "egg";
 import sqlconfig from "./sql.config.prod";
 export default () => {
   const config: PowerPartial<EggAppConfig> = {};
-  return { ...config, sequelize: sqlconfig };
+
+  const session = { domain: "serializedowen.com" };
+
+  return { ...config, session, sequelize: sqlconfig };
 };
