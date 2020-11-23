4b19f9e9173df83dbd0b769e7d5c83e80242af2c
serializedowen22 <wjh199455@gmail.com>
2020-11-23 23:28:25 +0800

    feat(image): intake crop parameters

--- packages/signin-gateway/server/app/controller/ImageController.ts
+++ packages/signin-gateway/server/app/controller/ImageController.ts
@@ -1,12 +1,18 @@
 import { Controller } from "egg";
-import { Post, Prefix } from "egg-shell-decorators-plus";
+import { Post, Prefix, Query } from "egg-shell-decorators-plus";
 import sharp from "sharp";
 // import qiniu from 'qiniu'
 
 @Prefix("/image")
 export default class ImageController extends Controller {
   @Post("/upload")
-  public async upload() {
+  public async upload(
+    @Query("x") x: String,
+    @Query("y") y: String,
+    @Query("width") width: String,
+    @Query("height") height: String
+  ) {
+    console.log(x, y, width, height);
     if (
       this.ctx.request.files.length === 0 ||
       this.ctx.request.files.length > 1
